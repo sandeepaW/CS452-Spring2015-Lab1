bfeb185e8fc849ecc2270375b802b8088b989b3b
sandeepaW <sandeepaW@users.noreply.github.com>
2015-01-30 15:31:51 -0500

    Update lab1.js

--- lab1.js
+++ lab1.js
@@ -1,7 +1,7 @@
 
 var gl;
 var points;
-
+var index=0;
 window.onload = function init()
 {
     var canvas = document.getElementById( "gl-canvas" );
@@ -10,11 +10,33 @@ window.onload = function init()
     if ( !gl ) { alert( "WebGL isn't available" ); }
 
 
+
     //
-    //  Configure WebGL
+ var vertices = [
+        vec2( -0.5, -0.5 ),
+        vec2(  -0.5,  0.5 ),
+        vec2(  0.5, 0.5 ),
+        vec2( 0.5, -0.5)
+    ];
+
+  
+ var vertices1 = new Float32Array([-1, -1, 0, 1, 1, -1]);
+
+     
+var vertices2 = [
+        vec2( -1, -1 ),
+        vec2(  -1,  1 ),
+        vec2(  1, 1),
+        vec2( 1, -1)
+    ];
+
+  
+//  Configure WebGL
+
+
     //
     gl.viewport( 0, 0, canvas.width, canvas.height );
-    gl.clearColor( 0.0, 0.0, 0.0, 1.0 );
+    gl.clearColor( 0.0, 0.0, 1.0, 1.0 );
     
     //  Load shaders and initialize attribute buffers
     
@@ -23,12 +45,98 @@ window.onload = function init()
     
     // Load the data into the GPU
 
-    // Associate our shader variables with our data buffer
 
-    render();
+var bufferId = gl.createBuffer();
+   
+ gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+  
+  gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
+
+  
+
+   
+ // Associate our shader variables with our data buffer
+ var vPosition = gl.getAttribLocation( program, "vPosition" );
+  
+  gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+ 
+   gl.enableVertexAttribArray( vPosition );
+
+   
+
+
+render();
+
+canvas.onclick=function() 
+{
+index=index+1;
+index=index % 3;
+if(index==0)
+{
+ var bufferId = gl.createBuffer();
+   
+ gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+  
+  gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
+
+  
+}
+else
+if(index==1)
+{
+var bufferId1 = gl.createBuffer();
+   
+ gl.bindBuffer( gl.ARRAY_BUFFER, bufferId1 );
+  
+  gl.bufferData( gl.ARRAY_BUFFER,flatten(vertices1), gl.STATIC_DRAW );
+
+
+
+}
+else 
+if(index==2)
+{
+ var bufferId2= gl.createBuffer();
+   
+ gl.bindBuffer( gl.ARRAY_BUFFER, bufferId2 );
+  
+  gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices2), gl.STATIC_DRAW );
+
+  
+}
+ var vPosition = gl.getAttribLocation( program, "vPosition" );
+  
+  gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+ 
+   gl.enableVertexAttribArray( vPosition );
+
+   
+render();
+}
+
+
 };
 
 
-function render() {
+function render() 
+{
     gl.clear( gl.COLOR_BUFFER_BIT );
+
+if (index==0)
+{
+
+gl.drawArrays( gl.TRIANGLE_FAN, 0, 4);
+
+}
+if(index==1)
+{
+
+gl.drawArrays( gl.TRIANGLES, 0, 3 );
+
+}
+if(index==2)
+{
+
+gl.drawArrays( gl.TRIANGLE_FAN, 0, 4 );
+}
 }
